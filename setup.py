01d07e1fa66d5397281db16b84a3e89c50dc7f71
Haris Muneer ⚡️ <haris.muneer5@gmail.com>
2020-05-22 12:56:26 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.6",
     extras_require={"dev": ["black", "twine", "wheel"],},
     install_requires=["selenium==3.141.0", "pyyaml", "webdriver_manager"],
     entry_points={
